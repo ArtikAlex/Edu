4f15d769eea8439b4f3f3271451adbb0a7d4603a
ArtikAlex/Erenford S21 <alexk5253@yandex.ru>
2022-12-27 11:04:28 +0300

    3

--- Operations_point/library.c
+++ Operations_point/library.c
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "library.h" //программа объясняет разницу между операциями над указателями
 
 int main(void) {
     int urns[5] = {100, 200, 300, 400, 500};
@@ -19,4 +19,11 @@ int main(void) {
     printf("ptr2 = %p, *ptr2 = %d, &ptr2 = %p\n", ptr2, *ptr2, &ptr2);
     --ptr1;
     ++ptr2;
+    printf("\nВосстановление исходных значений указателей:\n");
+    printf("ptr1 = %p, ptr2 = %p\n", ptr1, ptr2);
+    printf("\nВычитание одного указателя из другого:\n");
+    printf("ptr2 = %p, ptr1 = %p, ptr2-ptr1 = %td\n", ptr2, ptr1, ptr2-ptr1);
+    printf("\nВычитание из указателя значения типа int: \n");
+    printf("ptr3 = %p, ptr3 - 2 = &p\n", ptr3, ptr3-2);
+    return 0;
 }
